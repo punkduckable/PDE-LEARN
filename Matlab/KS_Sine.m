88f9db4841ddf266b6ef000e12923e37a6c68ca0
Robert Stephany <robert.stephany@utexas.edu>
2021-12-09 17:26:26 -0500

    Added scripts to generate data sets!
    I finally learned how to use chebfun's spin function. Armed with this
    knowledge, I wrote dataset generating scripts for the KS, KdV, Burgers,
    and 2d Heat equation. I previously used scripts from Raissi's deep
    hidden physics project (except for the 2d heat one)... I've been wanting
    to use my own scripts for quite some time... now I have them.

---
+++ Matlab/KS_Sine.m
@@ -0,0 +1,49 @@
+% KS equation: D_t U = -(D_x^2 U) - (D_x^4 U) - (D_x U)(U)
+% with U(x, 0) = -sin(pi*x/10);
+
+
+% Set up the problem domain. I want this to run on
+%       (t, x) in [0,50] x [-10, 10]
+x_l     = -10;
+x_h     = 10;
+t_l     = 0;
+t_h     = 50;
+Nt      = 251;
+Domain  = [x_l, x_h];
+T_span  = linspace(t_l, t_h, Nt);
+
+
+% Set up the spinop for Burger's equation. For this problem:
+%       L(u)    = -(D_x^2 U) - (D_x^4 U) 
+%       N(u)    = (U)(D_x U).
+%       Init(x) = -sin(pi*x/10)
+S           = spinop(Domain, T_span);
+S.lin       = @(u) -1.0*diff(u,2) - diff(u,4);
+S.nonlin    = @(u) -0.5*diff(u.^2);
+S.init      = chebfun(@(x) -sin(pi*x/10), Domain, 'vectorize');
+
+
+% Solve!
+disp("Solving...");
+Nx      = 256;
+Dt      = .0005;
+U_Cheb  = spin(S, N, Dt, 'plot', 'off');
+
+
+% Make the dataset....
+disp("Writing solution to array...");
+usol    = zeros(Nx, Nt);
+x_vals  = linspace(x_l, x_h, Nx + 1);
+x_range = x_vals(1:(end - 1));
+for t = 1:Nt
+    usol(:, t) = U_Cheb{t}(x_range);
+end
+
+% Save!
+disp("Saving...");
+t = T_span;
+x = x_range;
+save('ks_sine.mat','t','x','usol');
+
+% Plot!
+pcolor(t, x, usol); shading interp, axis tight, colormap(jet);
